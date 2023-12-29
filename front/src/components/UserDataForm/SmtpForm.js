af519538a77fecc808d5444cd3a4d8db484782f0
07791705478 <brunodedantas@gmail.com>
2023-12-29 15:57:23 -0300

    project

--- front/src/components/UserDataForm/SmtpForm.js
+++ front/src/components/UserDataForm/SmtpForm.js
@@ -1,5 +1,6 @@
 // SmtpForm.js
 import React, { useState } from 'react';
+import { FaGoogle, FaMicrosoft } from 'react-icons/fa';
 import './SmtpForm.css';
 
 const SmtpForm = () => {
@@ -23,89 +24,118 @@ const SmtpForm = () => {
     console.log('Configurações SMTP:', smtpSettings);
   };
 
+  const handleGoogleAuth = () => {
+    console.log('Autenticar com Google');
+    // Adicione a lógica de autenticação com o Google aqui
+  };
+
+  const handleOutlookAuth = () => {
+    console.log('Autenticar com Outlook');
+    // Adicione a lógica de autenticação com o Outlook aqui
+  };
+
   return (
     <div className="smtp-form-container">
+      <div className="title-container">
+        <h4>Servidor de e-mails SMTP</h4>
+      </div>
+      <div className="form-container">
         <div className="title-container">
-            <h4>Servidor de e-mails SMTP</h4>
+          <p>
+            A integração de e-mail por SMTP é habilitada de forma geral na conta e configurada
+            de forma individual, por endereço de e-mail.
+          </p>
+          <div className="oauth-buttons">
+            <button className="auth-button" onClick={handleGoogleAuth}>
+              <FaGoogle className="auth-icon" />
+              Autenticar com Google
+            </button>
+            <button className="auth-button" onClick={handleOutlookAuth}>
+              <FaMicrosoft className="auth-icon" />
+              Autenticar com Outlook
+            </button>
+          </div>
         </div>
-        <div className="form-container">
-            <p>A integração de e-mail por SMTP é habilitada de forma geral na conta e configurada
-                de forma individual, por endereço de e-mail.
-            </p>
-            <form onSubmit={handleSubmit}>
-                <div className="form-row">
-                    <div>
-                        <label htmlFor="username">Usuário:</label>
-                        <input
-                            type="text"
-                            id="username"
-                            name="username"
-                            value={smtpSettings.username}
-                            onChange={handleChange}
-                        />
-                    </div>
-                    <div>
-                        <label htmlFor="email">E-mail:</label>
-                        <input
-                            type="text"
-                            id="email"
-                            name="email"
-                            value={smtpSettings.email}
-                            onChange={handleChange}
-                        />
-                    </div>
-                </div>
+        <form onSubmit={handleSubmit}>
+          <div className="form-row">
+            <div>
+              <label htmlFor="username">Usuário: </label>
+              <input
+                type="text"
+                id="username"
+                name="username"
+                value={smtpSettings.username}
+                onChange={handleChange}
+                className="large-input"
+              />
+            </div>
+            <div>
+              <label htmlFor="email">E-mail: </label>
+              <input
+                type="text"
+                id="email"
+                name="email"
+                value={smtpSettings.email}
+                onChange={handleChange}
+                className="large-input"
+              />
+            </div>
+          </div>
 
-                <div className="form-row">
-                    <div>
-                        <label htmlFor="password">Senha:</label>
-                        <input
-                            type="password"
-                            id="password"
-                            name="password"
-                            value={smtpSettings.password}
-                            onChange={handleChange}
-                        />
-                    </div>
-                    <div>
-                        <label htmlFor="preConfig">Pré-config:</label>
-                        <input
-                            type="text"
-                            id="preConfig"
-                            name="preConfig"
-                            value={smtpSettings.preConfig}
-                            onChange={handleChange}
-                        />
-                    </div>
-                </div>
+          <div className="form-row">
+            <div>
+              <label htmlFor="password">Senha: </label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                value={smtpSettings.password}
+                onChange={handleChange}
+                className="large-input-ex-se"
+              />
+            </div>
+            <div>
+              <label htmlFor="preConfig">Pré-config: </label>
+              <input
+                type="text"
+                id="preConfig"
+                name="preConfig"
+                value={smtpSettings.preConfig}
+                onChange={handleChange}
+                className="large-input"
+              />
+            </div>
+          </div>
 
-                <div className="form-row">
-                    <div>
-                        <label htmlFor="host">Host:</label>
-                        <input
-                            type="text"
-                            id="host"
-                            name="host"
-                            value={smtpSettings.host}
-                            onChange={handleChange}
-                        />
-                    </div>
-                    <div>
-                        <label htmlFor="port">Porta:</label>
-                        <input
-                            type="text"
-                            id="port"
-                            name="port"
-                            value={smtpSettings.port}
-                            onChange={handleChange}
-                        />
-                    </div>
-                </div>
-                <div className="form-row">
-                <button type="submit">Salvar</button>
-                </div>
-            </form>
-        </div>
+          <div className="form-row">
+            <div>
+              <label htmlFor="host">Host: </label>
+              <input
+                type="text"
+                id="host"
+                name="host"
+                value={smtpSettings.host}
+                onChange={handleChange}
+                className="large-input-ex"
+              />
+            </div>
+            <div>
+              <label htmlFor="port">Porta: </label>
+              <input
+                type="text"
+                id="port"
+                name="port"
+                value={smtpSettings.port}
+                onChange={handleChange}
+                className="large-input"
+              />
+            </div>
+          </div>
+          <div className="form-row">
+            <button type="submit">Salvar</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
